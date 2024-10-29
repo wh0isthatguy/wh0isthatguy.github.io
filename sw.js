36e20c91d79307456aabc2bbff14828ffe7195b8
whoisthatguy <whoisthatguy.here@proton.me>
2024-10-29 03:40:04 -0700

    update

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1730198360815";
+const VERSION = "1730198404478";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
